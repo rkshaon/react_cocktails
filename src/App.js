8e47682812ae442b055531712baada1e95038b2e
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-01-15 16:51:43 +0600

    nav route

--- src/App.js
+++ src/App.js
@@ -9,9 +9,23 @@ import Error from './pages/Error'
 import Navbar from './components/Navbar'
 function App() {
   return (
-    <div>
-      <h2>app component</h2>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/cocktail/:id">
+          <SingleCocktail />
+        </Route>
+        <Route path="/*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
