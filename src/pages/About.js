7e6a67f9e934f76dd5beabb644a6ac253313497c
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-01-15 16:54:12 +0600

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis velit commodo, dignissim lectus eu, imperdiet dui. Suspendisse ornare vitae nisi eu vestibulum. Sed pretium est nec massa auctor, sit amet sagittis lectus scelerisque. Suspendisse quis viverra elit, eu volutpat est. Phasellus quis sagittis sem. Maecenas accumsan volutpat libero, sit amet egestas sapien malesuada at. Morbi tristique fermentum porta. Nunc sit amet finibus ipsum. Sed metus augue, fringilla quis auctor ac, commodo at ipsum. Curabitur sit amet velit nec lacus feugiat gravida in et ante. Etiam ullamcorper ex dapibus urna maximus suscipit. Nam tincidunt, sem eget cursus maximus, nisl nisl viverra risus, mollis scelerisque diam sem sed nibh. Vestibulum in dolor elementum, accumsan leo ut, vulputate ante.</p>
+    </section>
   )
 }
 
