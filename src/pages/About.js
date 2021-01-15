37090e1aa519da36efa8e3e91dea89678a2c6faa
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-01-15 16:58:01 +0600

    error page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section className="section about-section">
       <h1 className="section-title">about page</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis velit commodo, dignissim lectus eu, imperdiet dui. Suspendisse ornare vitae nisi eu vestibulum. Sed pretium est nec massa auctor, sit amet sagittis lectus scelerisque. Suspendisse quis viverra elit, eu volutpat est. Phasellus quis sagittis sem. Maecenas accumsan volutpat libero, sit amet egestas sapien malesuada at. Morbi tristique fermentum porta. Nunc sit amet finibus ipsum. Sed metus augue, fringilla quis auctor ac, commodo at ipsum. Curabitur sit amet velit nec lacus feugiat gravida in et ante. Etiam ullamcorper ex dapibus urna maximus suscipit. Nam tincidunt, sem eget cursus maximus, nisl nisl viverra risus, mollis scelerisque diam sem sed nibh. Vestibulum in dolor elementum, accumsan leo ut, vulputate ante.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis velit commodo, dignissim lectus eu, imperdiet dui. Suspendisse ornare vitae nisi eu vestibulum. Sed pretium est nec massa auctor, sit amet sagittis lectus scelerisque. Suspendisse quis viverra elit, eu volutpat est. Phasellus quis sagittis sem. Maecenas accumsan volutpat libero, sit amet egestas sapien malesuada at. Morbi tristique fermentum porta. Nunc sit amet finibus ipsum.</p>
     </section>
   )
 }
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -3,9 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
-    <div>
-      <h2>error page</h2>
-    </div>
+    <section className="error-page section">
+      <div className="error-container">
+        <h1>Oops! it's a dead end...</h1>
+        <Link to="/" className="btn btn-primary">back to home</Link>
+      </div>
+    </section>
   )
 }
 
