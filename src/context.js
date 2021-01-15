f7b284ad4b21c9420d5e64390148b66f3af71a72
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-01-15 22:43:37 +0600

    dependency

--- src/context.js
+++ src/context.js
@@ -9,7 +9,7 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState('a');
   const [cocktails, setCocktails] = useState([]);
 
-  const fetchDrinks = async () => {
+  const fetchDrinks = useCallback(async () => {
     setLoading(true);
     try {
       const response = await fetch(`${url}${searchTerm}`);
@@ -39,11 +39,11 @@ const AppProvider = ({ children }) => {
       console.log(error);
       setLoading(false);
     }
-  }
+  }, [searchTerm]);
 
   useEffect(() => {
     fetchDrinks();
-  }, [searchTerm]);
+  }, [searchTerm, fetchDrinks]);
 
   return(
     <AppContext.Provider
